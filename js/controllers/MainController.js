22f4665e67f0991e7b3623ea8fe1d6e1bd6c0b9a
Tavo <gariosp@unal.edu.co>
2017-08-16 17:00:37 -0300

    Directives

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,6 +13,18 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 8, 
       pubdate: new Date('2013', '08', '01'), 
       cover: 'img/program-or-be-programmed.jpg' 
+  	},
+    { 
+      name: 'Cien anios de soledad', 
+      price: 13, 
+      pubdate: new Date('1967', '06', '05'), 
+      cover: 'img/cien-anios-de-soledad.jpg' 
+  	},
+    { 
+      name: 'La divina comedia', 
+      price: 10, 
+      pubdate: new Date('1472', '04', '11'), 
+      cover: 'img/la-divina-comedia.jpg' 
   	}
   ]
 }]);
