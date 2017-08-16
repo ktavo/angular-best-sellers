f09618d08a6668de21b2166244eda2733acdf293
Tavo <gariosp@unal.edu.co>
2017-08-16 17:21:57 -0300

    ng-click II

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,30 +7,37 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 19, 
       pubdate: new Date('2014', '03', '08'), 
       cover: 'img/the-book-of-trees.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }, 
     { 
       name: 'Program or be Programmed', 
       price: 8, 
       pubdate: new Date('2013', '08', '01'), 
       cover: 'img/program-or-be-programmed.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     { 
       name: 'Cien anios de soledad', 
       price: 13, 
       pubdate: new Date('1967', '06', '05'), 
       cover: 'img/cien-anios-de-soledad.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     { 
       name: 'La divina comedia', 
       price: 10, 
       pubdate: new Date('1472', '04', '11'), 
       cover: 'img/la-divina-comedia.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }]
   $scope.plusOne = function(index) {
     $scope.products[index].likes += 1;
+  };
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
   };
 }]);
