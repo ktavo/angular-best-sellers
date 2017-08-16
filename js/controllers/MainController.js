6cff61365008a9a56c4de3e8b1fc1e920a706171
Tavo <gariosp@unal.edu.co>
2017-08-16 16:10:58 -0300

    Hello Angular JS II

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,4 @@
 app.controller("MainController", ['$scope', function($scope){
-  $scope.title = 'Top Sellers in Books';
+  $scope.title = 'Top Sellers';
+  $scope.promo = 'This one is on promo';
 }]);
